86fda8ffe4b7d7ab7cb5dc70b40322931e67fa8c
Lanzoor <lanzoorgaming@gmail.com>
2026-04-24 19:49:07 +0900

    fnlksaflbadsfbasdlkfbasdkfjjsdabkjasgb faskvf asdh basdkjhsbda

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import { NextConfig } from 'next';
+import type { NextConfig } from 'next';
 
-const nextConfig = {
-    reactStrictMode: false,
+const routes = ['status', 'discord-status', 'vscode-tweaks/ping', 'vscode-tweaks/weather'];
+
+const nextConfig: NextConfig = {
+    async rewrites() {
+        return routes.map((route) => ({
+            source: `/${route}`,
+            destination: `/api/${route}`,
+        }));
+    },
 };
 
 export default nextConfig;
