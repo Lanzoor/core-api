994ac586857ae0d14112f0b6675d506e4ce77ea7
Lanzoor <lanzoorgaming@gmail.com>
2026-04-25 21:38:24 +0900

    Stage commits for v26.1.0: add basic structure for page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,60 @@
-import './globals.css';
+'use client';
+
+import { useState, useEffect } from 'react';
+
 import './page.css';
 
+const themes: string[] = ['dark', 'light'];
+
 export default function Page() {
+    const [theme, setTheme] = useState('dark');
+
+    useEffect(() => {
+        const stored = localStorage.getItem('theme');
+
+        if (stored && themes.includes(stored)) {
+            setTheme(stored);
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('theme', theme);
+        document.documentElement.setAttribute('data-theme', theme);
+    }, [theme]);
+
+    function toggleTheme() {
+        setTheme((prev) => (prev === 'dark' ? 'light' : 'dark'));
+    }
+
     return (
-        <main>
+        <div className="container">
+            <nav>
+                <a href="https://api.lanzoor.dev">home</a>
+                <a href="https://api.lanzoor.dev/status">status</a>
+                <a href="https://api.lanzoor.dev/docs">docs</a>
+                <a href="https://lanzoor.dev">website</a>
+            </nav>
+
             <h1>api.lanzoor.dev</h1>
 
+            <hr />
+
             <p>
-                Welcome to api.lanzoor.dev!
+                welcome to <code>api.lanzoor.dev</code>! 👋
                 <br />
-                Check out{' '}
-                <a href="https://api.lanzoor.dev/docs">
-                    <code>/docs</code>
-                </a>{' '}
-                for more information.
+                this section of the website contains some API function routes that I often use
                 <br />
-                Status: <code>/status</code>
+                expect things to be more experimental than production-ready
+                <br />
+                <b>click one of the links above to get started!</b>
             </p>
-        </main>
+
+            <button onClick={toggleTheme}>
+                <img
+                    src="/icons/color-mode.svg"
+                    alt="theme"
+                />
+            </button>
+        </div>
     );
 }
