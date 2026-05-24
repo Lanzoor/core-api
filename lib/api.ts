fe17a1bcfaa4633a904408a714ea94348f543a31
Lanzoor <lanzoorgaming@gmail.com>
2026-05-24 18:27:08 +0900

    Update version tags

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import { NextResponse, NextRequest } from 'next/server';
 
 export const coreVersions: Record<any, string> = {
-    frontend: 'v26.22.0',
+    frontend: 'v26.22.1',
     backend: 'v26.1.9',
 };
 
