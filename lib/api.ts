22f125fb9b594f6232b7973cbb251db196f53f34
Lanzoor <lanzoor-programming@proton.me>
2026-08-06 21:49:23 +0900

    core-api :: update version tags

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import { NextResponse, NextRequest } from 'next/server';
 
 export const coreVersions: Record<any, string> = {
-    frontend: 'v26.27.4',
+    frontend: 'v26.27.5',
     backend: 'v26.2.0',
 };
 
