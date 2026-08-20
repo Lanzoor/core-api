54e29f05e321afd82420e8e34e473e1c129338f8
Lanzoor <lanzoor-programming@proton.me>
2026-08-20 20:47:27 +0900

    core-api :: unify file-system permissions & update version tags

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import { NextResponse, NextRequest } from 'next/server';
 
 export const coreVersions: Record<any, string> = {
-    frontend: 'v26.28.1',
+    frontend: 'v26.28.2',
     backend: 'v26.2.0',
 };
 
